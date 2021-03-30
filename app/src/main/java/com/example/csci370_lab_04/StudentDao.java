89b150c0da6a67340a82ceb1261ec8db785a9b27
Jacob <manningjc@g.cofc.edu>
2021-03-30 19:06:51 -0400

    Add Classes

---
+++ app/src/main/java/com/example/csci370_lab_04/StudentDao.java
@@ -0,0 +1,14 @@
+package com.example.csci370_lab_04;
+
+public class StudentDao {
+
+    public StudentDao() {
+
+        Student studentone;
+        Student studenttwo;
+        Student studentthree;
+        Student studentfour;
+        Student studentfive;
+    }
+
+}
